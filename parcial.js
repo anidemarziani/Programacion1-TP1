ba6db38df93b0f79872ff7a61ba7a893e2b6fa88
Ana Demarziani <Ana_demarziani@hotmail.com>
2024-10-12 19:57:03 -0300

    Ajustes

--- parcial.js
+++ parcial.js
@@ -20,7 +20,7 @@ const cargarNuevoDisco = () => {
                
                 pistas = [];
                 let cargarPista = true;
-            
+                let duracionDisco  = 0;
                 while (cargarPista) {
                     let nombrePista, duracionPista;
                     
@@ -31,6 +31,7 @@ const cargarNuevoDisco = () => {
                         if(duracionPista !=  null ) // Valida si se cancelo el prompt
                         {
                             pistas.push({ nombre: nombrePista, duracion: duracionPista });
+                            duracionDisco+=duracionPista;
                         }
                             
                     }
@@ -38,7 +39,7 @@ const cargarNuevoDisco = () => {
                     cargarPista = confirm("¿Desea ingresar otra pista?");
                 }
 
-                discos.push({ nombre: nombreDisco, autor: autorBanda, codigo: codigoUnico, pistas: pistas });
+                discos.push({ nombre: nombreDisco, autor: autorBanda, codigo: codigoUnico, duracion:duracionDisco , pistas: pistas });
                 
                 document.getElementById("totalRecopilado").innerHTML  = `<div><p>Tenes recopilado/s un total de ${discos.length} disco/s!</p></div> `;
             }
@@ -48,40 +49,38 @@ const cargarNuevoDisco = () => {
 };
 // Función Mostrar:
 const mostrarDiscos = () => {
-    // Variable para ir armando la cadena:
+    if( document.getElementById("discosNoEncontrados").classList.contains("hide") == false) { document.getElementById("discosNoEncontrados").classList.add("hide");} 
+    document.getElementById("discosNoEncontrados").classList.add("hide");
+    document.getElementById("totalRecopilado").innerHTML  = `<div><p>Tenes recopilado/s un total de ${discos.length} disco/s!</p></div> `;
     
     let discosHtml = "";
-
-    if( discos.length == 0 && document.getElementById("discosNoEncontrados").classList.contains("hide")) 
-    {
-        document.getElementById("discosNoEncontrados").classList.remove("hide");
-    }else
-    {
-        document.getElementById("discosNoEncontrados").classList.add("hide");
-
-        document.getElementById("totalRecopilado").innerHTML  = `<div><p>Tenes recopilado/s un total de ${discos.length} disco/s!</p></div> `;
-        discos.forEach(disco => {
-            let duracionMaxima = 0;
-            let pistaDuracionMaxima = null;
-            let duracionTotalDisco = 0;
-    
-            // Encontrar la pista con la duración máxima y calcular la duración total del disco
-            disco.pistas.forEach(pista => {
-                duracionTotalDisco += pista.duracion;
-                if (pista.duracion > duracionMaxima) {
-                    duracionMaxima = pista.duracion;
-                    pistaDuracionMaxima = pista;
-                }
-            });
-    
-            let promedioDuracion = duracionTotalDisco / disco.pistas.length;
-            if( isNaN(promedioDuracion) )promedioDuracion = 0;
-            discosHtml += CrearTarjetaDisco(disco, duracionTotalDisco,promedioDuracion,pistaDuracionMaxima);
-    
-        });
-        
-        document.getElementById("info").innerHTML = discosHtml;
+    let discoMayorDuracionCodigoUnico;
+    if( discos.length > 1){
+        discoMayorDuracionCodigoUnico = discos.sort( (a,b)=> b.duracion - a.duracion)[0].codigo;
     }
+
+    discos.forEach(disco => {
+        let duracionMaxima = 0;
+        let pistaDuracionMaxima = null;
+        // Encontrar la pista con la duración máxima y calcular la duración total del disco
+        disco.pistas.forEach(pista => {
+            if (pista.duracion > duracionMaxima) {
+                duracionMaxima = pista.duracion;
+                pistaDuracionMaxima = pista;
+            }
+        });
+
+        let promedioDuracion = disco.duracion / disco.pistas.length;
+        let esDiscoDeMayorDuracion = false;
+        if( disco.codigo === discoMayorDuracionCodigoUnico ) esDiscoDeMayorDuracion = true;
+        if( isNaN(promedioDuracion) ) promedioDuracion = 0;
+
+        discosHtml += CrearTarjetaDisco(disco,promedioDuracion,pistaDuracionMaxima, esDiscoDeMayorDuracion);
+
+    });
+    
+    document.getElementById("info").innerHTML = discosHtml;
+
 
 
 }
@@ -107,26 +106,28 @@ const BuscarPorCodigo = () => {
         }
     } while ( codigoBuscado !== null && ( isNaN(codigoBuscado) || codigoBuscado < 1 || codigoBuscado > 999 ) ) ;
     
-    if(codigoBuscado !== null ){
+    if(codigoBuscado !== null )
+    {
+        
+        document.getElementById("totalRecopilado").innerHTML  = `<div><p>Resultados de búsqueda: ${codigoBuscado}</p></div> `;
         const discoEncontrado = discos.find(disco => disco.codigo === codigoBuscado);
 
         if (discoEncontrado) {
             let duracionMaxima = 0;
             let pistaDuracionMaxima = null;
-            let duracionTotalDisco = 0;
-    
+            let esDiscoDeMayorDuracion = false;
+            
             discoEncontrado.pistas.forEach(pista => {
-                duracionTotalDisco += pista.duracion;
                 if (pista.duracion > duracionMaxima) {
                     duracionMaxima = pista.duracion;
                     pistaDuracionMaxima = pista;
                 }
             });
     
-            let promedioDuracion = duracionTotalDisco / discoEncontrado.pistas.length;
-            if( isNaN(promedioDuracion) )promedioDuracion =0;
+            let promedioDuracion = discoEncontrado.duracion / discoEncontrado.pistas.length;
+            if( isNaN(promedioDuracion) )promedioDuracion = 0;
     
-            let html = CrearTarjetaDisco(discoEncontrado, duracionTotalDisco,promedioDuracion,pistaDuracionMaxima);
+            let html = CrearTarjetaDisco(discoEncontrado,promedioDuracion,pistaDuracionMaxima, esDiscoDeMayorDuracion);
         
             document.getElementById("discosNoEncontrados").classList.add("hide");
             document.getElementById("info").innerHTML = html;
@@ -142,29 +143,30 @@ const BuscarPorCodigo = () => {
 };
 
 // Funcion Crear Tarjeta Disco: Devuelve el html que representa la tarjeta del disco.
-const CrearTarjetaDisco = ( disco,duracionTotalDisco, promedioDuracion , pistaDeMayorDuracion) => {
-    let template = `<article id="${disco.codigo}">
-    <h3>${disco.nombre}</h3>
-    <p><strong>Autor/Banda</strong><br> ${disco.autor}</p>
-    <p><strong>Código único</strong><br> ${disco.codigo}</p>
-    <p><strong>Cantidad de pistas</strong><br> ${disco.pistas.length}</p>
-    `;
+const CrearTarjetaDisco = ( disco, promedioDuracion, pistaDeMayorDuracion, discoMayorDuracion) => {
+    let template = `<li class="disco" id="${disco.codigo}">
+    <h3>${disco.nombre}</h3>`
+
+    if( discoMayorDuracion == true) template += `<p class="mayorDuracion">Mayor duración</p>`;
+
+    template += `<p class="autor"><strong>Autor/Banda</strong><br> ${disco.autor}</p>
+        <p><strong>Código único</strong><br> ${disco.codigo}</p>
+        <p><strong>Cantidad de pistas</strong><br> ${disco.pistas.length}</p>
+    `;  
 
     if( disco.pistas.length > 0){
-        template += `<p><strong>Pista de mayor duracion</strong> <br>${pistaDeMayorDuracion.nombre}</p>
-            <p><strong>Duración total del disco</strong> <br>${duracionTotalDisco} segundos</p>
+        template += `<p><strong>Pista de mayor duración</strong> <br>${pistaDeMayorDuracion.nombre}</p>
+            <p><strong>Duración total del disco</strong> <br>${disco.duracion} segundos</p>
             <p><strong>Promedio de duración</strong><br>${promedioDuracion.toFixed(2)} segundos</p>
             <h4>Pistas:</h4><ol>`
             
         disco.pistas.forEach(pista => {
-            let duracionEnRojo = pista.duracion > 180 ? `<span class="duracionEnRojo">(${pista.duracion}s)</span>` : pista.duracion;
-            template += `<li> ${pista.nombre} ${duracionEnRojo}</li>`;
+            let duracionEnRojo = pista.duracion > 180 ? `<span class="duracionEnRojo">${pista.duracion}s</span>` : `<span>${pista.duracion}s</span>`;
+            template += `<li> <span>${pista.nombre}</span> - ${duracionEnRojo}</li>`;
         });
-        template += "</ol></article>";
+        template += "</ol></li>";
     }
-
-
-    
+   
     return template;
 }
 // Función Obtener nombre del disco: Itera hasta obtener un nombre de disco valido, o su la cancelacion del prompt que devuelve null.
@@ -172,6 +174,7 @@ const ObtenerNombreDelDisco = () => {
     let nombreDisco="";
     do {
         nombreDisco = prompt("Ingrese el nombre del disco:");
+        if ( nombreDisco !== null) nombreDisco = nombreDisco.trim();
         if (nombreDisco === "") {
             alert("El nombre del disco no puede estar vacío.");
         }
@@ -184,6 +187,7 @@ const ObtenerNombreDeLaBanda = () => {
     let autorBanda = "";
     do {
         autorBanda = prompt("Ingrese el autor o banda del disco:");
+        if ( autorBanda !== null) autorBanda = autorBanda.trim();
         if (autorBanda === "") {
             alert("El autor o banda del disco no puede estar vacío.");
         }
@@ -196,6 +200,7 @@ const ObtenerCodigoUnicoDelDisco = ()=> {
     do {
         codigoUnico = prompt("Ingrese el código numérico único del disco (entre 1 y 999):");
         if( codigoUnico !== null) {
+            codigoUnico = codigoUnico.trim();
             codigoUnico = parseInt(codigoUnico);
             if (isNaN(codigoUnico) || codigoUnico < 1 || codigoUnico > 999) {
                 if (isNaN(codigoUnico)) {
@@ -215,6 +220,7 @@ const ObtenerNombrePista = () => {
     let nombrePista;
     do {
         nombrePista = prompt("Ingrese el nombre de la pista:");
+        if ( nombrePista !== null) nombrePista = nombrePista.trim();
         if (nombrePista === "") {
             alert("El nombre de la pista no puede estar vacío.");
         }
@@ -226,8 +232,11 @@ const ObtenerDuracionPista = () => {
     let duracionPista;
     do {
         duracionPista = prompt("Ingrese la duración de la pista en segundos (entre 0 y 7200):");
-        if( duracionPista !== null) 
+        if( duracionPista !== null) {
+            duracionPista = duracionPista.trim();
             duracionPista = parseInt(duracionPista);
+        }
+            
         if (isNaN(duracionPista) || duracionPista < 0 || duracionPista > 7200) {
             alert("La duración de la pista debe ser un número entre 0 y 7200 segundos.");
         }
